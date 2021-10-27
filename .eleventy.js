8659c7c146bf0ea78f35de79c771ea32fd68a7c5
roobottom <roo@roodesign.co.uk>
2021-10-27 10:34:38 +0100

    better organisation of files

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,23 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy")
+
 module.exports = function (eleventyConfig) {
 
   //collections
-  eleventyConfig.addCollection('articles', require('./_lib/collections/articles.js'))
-  eleventyConfig.addCollection('archives', require('./_lib/collections/archives.js'))
+  eleventyConfig.addCollection('articles', require('./lib/collections/articles.js'))
+  eleventyConfig.addCollection('archives', require('./lib/collections/archives.js'))
 
   //shortcodes
-  eleventyConfig.addShortcode('figure', require('./_lib/shortcodes/figure.js'))
+  eleventyConfig.addShortcode('figure', require('./lib/shortcodes/figure.js'))
 
   //filters
-  eleventyConfig.addFilter('date', require('./_lib/filters/date.js'))
-  eleventyConfig.addFilter('slug_date', require('./_lib/filters/slug-date.js'))
+  eleventyConfig.addFilter('date', require('./lib/filters/date.js'))
+  eleventyConfig.addFilter('slug_date', require('./lib/filters/slug-date.js'))
 
   return {
     dir: {
-      input: '_source',
-      includes: '_includes',
-      layouts: '_layouts',
+      input: 'source',
+      includes: 'includes',
+      layouts: 'layouts',
       dataTemplateEngine: 'njk',
       markdownTemplateEngine: 'njk',
       htmlTemplateEngine: 'njk',
