4577d2b9fa91787c6bd0277bc31447f5a3bb1311
roobottom <roo@roodesign.co.uk>
2021-11-16 22:02:14 +0000

    updated photos data

--- source/content/photos/photos.11tydata.js
+++ source/content/photos/photos.11tydata.js
@@ -1,13 +1,21 @@
-const moment = require('moment')
+const slugDate = require('../../../lib/filters/slug-date.js')
+const date = require('../../../lib/filters/date.js')
+const openGraph = require('../../data/openGraph.js')
 
 module.exports = {
+  layout: 'page.njk',
   eleventyComputed: {
     title: data => { 
-      if (data.summary) {
-        return data.summary
-      } else {
-        return moment(data.date).format('D MMMM YYYY')
+      return data.summary === undefined || data.summary === '' ? `Diary entry for ${date(data.date)}` : data.summary
+    },
+    permalink: data => `/diary/${slugDate(data.date)}-${data.page.fileSlug}/`,
+    openGraph: data => {
+      return {
+        type: 'article',
+        description: data.summary === undefined ? 'Another diary post from the keyboard of Jon Roobottom' : data.summary,
+        image: data.photo[0].url === undefined ? openGraph.image : `https://ik.imagekit.io/roobottom/tr:w-1280,h-640,fo-auto/${data.photo[0].url}`,
+        imageAlt: data.photo[0].alt === undefined ? `The image for the diary post titled ${data.title}` : data.photo[0].alt
       }
-     }
+    }
   }
 }
