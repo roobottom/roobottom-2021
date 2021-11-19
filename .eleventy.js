a521fce05f12d7d8a80b243986cb4ac1a690e52f
roobottom <roo@roodesign.co.uk>
2021-11-19 22:19:07 +0000

    more shiz

--- .eleventy.js
+++ .eleventy.js
@@ -52,6 +52,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('subset', require('./lib/filters/subset.js'))
   eleventyConfig.addFilter("plural", require('./lib/filters/plural.js'))
   eleventyConfig.addFilter("slugify", require('./lib/filters/slugify.js'))
+  eleventyConfig.addFilter("hangingPunctuation", str => {
+    return str.replace(/^(<p>)*([“"])(.*)/gm,"$1<span class='hanging-punctuation'>$2</span>$3")
+  })
 
 //** static files
   eleventyConfig.addPassthroughCopy({ "source/content/admin/*.yml": "admin/" })
