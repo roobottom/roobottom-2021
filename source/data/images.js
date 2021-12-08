f1feb85f316c111bc2a259dd790fb5755bab49d8
roobottom <roo@roodesign.co.uk>
2021-12-08 23:03:35 +0000

    publish exif data for images

--- source/data/images.js
+++ source/data/images.js
@@ -1,21 +1,39 @@
+require('dotenv').config()
 const glob = require("glob")
 const path = require("path")
+const exiftool = require("exiftool-vendored").exiftool
 const base = 'source/'
 
-async function getImages () {
-  let thefiles = []
-  glob(`${base}**/*.{jpg,jpeg}`, function (er, files) {
-    for (file of files) {
-      thefiles.push({
-        file: file,
-        ...path.parse(file),
-        relative: file.replace(base, '')
-      })
-    }
-  })
-  return thefiles
+async function getExif (file) {
+  return exiftool
+    .read(file)
+    .then((tags) => {
+      return tags
+    })
 }
 
-module.exports = async function () {
+async function getImages () {
+
+  const images = glob.sync(`${base}**/*.{jpg,jpeg}`)
+  let enrichedImages = []
+  
+  for (image of images) {
+    /* getting exif data is very costly, only do that in production */
+    //let exif = process.env.NODE_ENV === 'dev' ? {} : await getExif(image)
+    let exif = await getExif(image)
+
+    enrichedImages.push({
+      file: image,
+      relative: image.replace(base, ''),
+      ...path.parse(image),
+      exif: exif
+    })
+  }
+
+  return enrichedImages
+
+}
+
+module.exports = function () {
   return getImages()
 }
