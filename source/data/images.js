2c6d64cc83d440af39a166add80c361686f2bc4e
roobottom <roo@roodesign.co.uk>
2021-12-09 23:34:41 +0000

    maps.

--- source/data/images.js
+++ source/data/images.js
@@ -21,6 +21,10 @@ async function getImages () {
     /* getting exif data is very costly, only do that in production */
     //let exif = process.env.NODE_ENV === 'dev' ? {} : await getExif(image)
     let exif = await getExif(image)
+    let gps = exif.GPSLatitude && exif.GPSLongitude ? {
+      latitude: exif.GPSLatitude,
+      longitude: exif.GPSLongitude,
+    } : null
 
     enrichedImages.push({
       file: image,
@@ -28,11 +32,7 @@ async function getImages () {
       relativeDir: path.parse(image).dir.replace(base, ''),
       ...path.parse(image),
       exif: {
-        gps: {
-          latitude: exif.GPSLatitude,
-          longitude: exif.GPSLongitude,
-
-        },
+        gps: gps,
         settings: {
           iso: exif.ISO,
           speed: exif.ShutterSpeedValue,
@@ -44,7 +44,6 @@ async function getImages () {
           lens: exif.LensModel
         },
         file: {
-          size: exif.FileSize,
           date: exif.CreateDate
         }
       }
