1d10d5d60a8bf042966d9a72a30c84c128d0650f
roobottom <roo@roodesign.co.uk>
2021-12-16 23:41:07 +0000

    better atom feed

--- source/data/global.js
+++ source/data/global.js
@@ -3,8 +3,11 @@ module.exports = function () {
         dob: process.env.DOB,
         env: process.env.NODE_ENV,
         github_repo: process.env.GITHUB_REPO,
+        site_title: 'Jon’s website',
+        site_description: 'My personal website where I write about my life; it’s of limited interest to anyone.',
         base_url: "https://roobottom.com",
         robots: "/robots.txt",
-        sitemap: "/sitemap.txt"
+        sitemap: "/sitemap.txt",
+        author: "Jon Roobottom"
     }
 }
