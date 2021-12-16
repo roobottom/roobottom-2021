c340e2915565a266340e58b2d758d5844b668937
roobottom <roo@roodesign.co.uk>
2021-12-16 22:04:17 +0000

    include all pages in sitemap

--- source/content/sitemap.11ty.js
+++ source/content/sitemap.11ty.js
@@ -3,13 +3,17 @@ class SiteMap {
     return {
       eleventyExcludeFromCollections: true,
       permalink: function (data) {
-        return data.global.sitemap;
+        return data.global.sitemap
       },
     };
   }
 
-  render({ collections: { all: content }, global: { base_url } }) {
-    const urls = content.map((c) => `${base_url}${c.url}`)
+  render({ collections: { content, years, tags }, global: { base_url } }) {
+    const navigation = require('../data/globalNavigation.js').map(item => `${base_url}${item.href}`)
+    const posts = content.map(post => `${base_url}${post.url}`)
+    const archive = years.map(year => `${base_url}/archives/${year}`)
+    const tagsList = tags.map(tag => `${base_url}/tags/${tag.title}`)
+    const urls = [...navigation, ...posts, ...archive, ...tagsList]
     urls.sort()
     return urls.join("\n")
   }
